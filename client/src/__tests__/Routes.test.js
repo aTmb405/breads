0d91d6ca6a8809ad762399704e40af53bf19f4d3
aTmb405 <aTmb405@gmail.com>
2020-04-11 17:33:39 -0500

    added basic tests to main components

--- client/src/__tests__/Routes.test.js
+++ client/src/__tests__/Routes.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './containers/App';
+import Routes from '../containers/Routes';
 
 it('renders without crashing', () => {
-    shallow(<App />);
+    shallow(<Routes />);
 });
