6c04555d9e2926e9606c0060afb6d977bd8dfbd1
aTmb405 <aTmb405@gmail.com>
2020-04-17 12:36:22 -0500

    Add homepage to package.json

---
+++ client/build/precache-manifest.b396ef050410da86890d432d5c5e9b8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "653a4a1b65ba88400c6f5deb1c58e150",
+    "url": "/www.breads.io/index.html"
+  },
+  {
+    "revision": "facb19566b0a2594e1be",
+    "url": "/www.breads.io/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f7e49d9aecec2d82a2e6",
+    "url": "/www.breads.io/static/css/main.aa8d2e26.chunk.css"
+  },
+  {
+    "revision": "facb19566b0a2594e1be",
+    "url": "/www.breads.io/static/js/2.08344ab9.chunk.js"
+  },
+  {
+    "revision": "b4ee14f1b0eb12541a4fed8ba73fd856",
+    "url": "/www.breads.io/static/js/2.08344ab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7e49d9aecec2d82a2e6",
+    "url": "/www.breads.io/static/js/main.54df3db6.chunk.js"
+  },
+  {
+    "revision": "febcac1cc189c82bee09",
+    "url": "/www.breads.io/static/js/runtime-main.9d1ba1cf.js"
+  }
+]);
